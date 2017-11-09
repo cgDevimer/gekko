1e4a7f02da84687f78be16dbb5503795e0546b32
raminerfani <ramin.erfani@annalect.com>
2017-11-09 19:26:14 +0100

    First commit

--- strategies/custom.js
+++ strategies/custom.js
@@ -1,62 +1,101 @@
-// This is a basic example strategy for Gekko.
-// For more information on everything please refer
-// to this document:
-//
-// https://gekko.wizb.it/docs/strategies/creating_a_strategy.html
-//
-// The example below is pretty bad investment advice: on every new candle there is
-// a 10% chance it will recommend to change your position (to either
-// long or short).
+/*
 
-var log = require('../core/log');
+  RSI - cykedev 14/02/2014
 
-// Let's create our own strat
-var strat = {};
+  (updated a couple of times since, check git history)
 
-// Prepare everything our method needs
-strat.init = function() {
-  this.currentTrend = 'long';
-  this.requiredHistory = 0;
+ */
+// helpers
+var _ = require('lodash');
+var log = require('../core/log.js');
+
+var RSI = require('./indicators/RSI.js');
+
+// let's create our own method
+var method = {};
+
+// prepare everything our method needs
+method.init = function() {
+  this.name = 'RSI';
+
+  this.trend = {
+    direction: 'none',
+    duration: 0,
+    persisted: false,
+    adviced: false
+  };
+
+  this.requiredHistory = this.tradingAdvisor.historySize;
+
+  // define the indicators we need
+  this.addIndicator('rsi', 'RSI', this.settings);
 }
 
-// What happens on every new candle?
-strat.update = function(candle) {
+// for debugging purposes log the last
+// calculated parameters.
+method.log = function(candle) {
+  var digits = 8;
+  var rsi = this.indicators.rsi;
 
-  // Get a random number between 0 and 1.
-  this.randomNumber = Math.random();
-
-  // There is a 10% chance it is smaller than 0.1
-  this.toUpdate = this.randomNumber < 0.1;
+  log.debug('calculated RSI properties for candle:');
+  log.debug('\t', 'rsi:', rsi.result.toFixed(digits));
+  log.debug('\t', 'price:', candle.close.toFixed(digits));
 }
 
-// For debugging purposes.
-strat.log = function() {
-  log.debug('calculated random number:');
-  log.debug('\t', this.randomNumber.toFixed(3));
-}
+method.check = function() {
+  var rsi = this.indicators.rsi;
+  var rsiVal = rsi.result;
 
-// Based on the newly calculated
-// information, check if we should
-// update or not.
-strat.check = function() {
+  if(rsiVal > this.settings.thresholds.high) {
 
-  // Only continue if we have a new update.
-  if(!this.toUpdate)
-    return;
+    // new trend detected
+    if(this.trend.direction !== 'high')
+      this.trend = {
+        duration: 0,
+        persisted: false,
+        direction: 'high',
+        adviced: false
+      };
 
-  if(this.currentTrend === 'long') {
+    this.trend.duration++;
 
-    // If it was long, set it to short
-    this.currentTrend = 'short';
+    log.debug('In high since', this.trend.duration, 'candle(s)');
+
+    if(this.trend.duration >= this.settings.thresholds.persistence)
+      this.trend.persisted = true;
+
+  } else if(rsiVal <= this.settings.thresholds.high && this.trend.direction === 'high' && this.trend.persisted && !this.trend.adviced) {
+    this.trend.adviced = true;
     this.advice('short');
+    log.debug('Go short');
+  } else if(rsiVal < this.settings.thresholds.low) {
 
+    // new trend detected
+    if(this.trend.direction !== 'low')
+      this.trend = {
+        duration: 0,
+        persisted: false,
+        direction: 'low',
+        adviced: false
+      };
+
+    this.trend.duration++;
+
+    log.debug('In low since', this.trend.duration, 'candle(s)');
+
+    if(this.trend.duration >= this.settings.thresholds.persistence)
+      this.trend.persisted = true;
+
+
+  } else if(rsiVal >= this.settings.thresholds.low && this.trend.direction === 'low' && this.trend.persisted && !this.trend.adviced) {
+    this.trend.adviced = true;
+    this.advice('long');
+    log.debug('Go long');
   } else {
 
-    // If it was short, set it to long
-    this.currentTrend = 'long';
-    this.advice('long');
-
+    log.debug('In no trend');
+    this.advice();
   }
 }
 
-module.exports = strat;
+module.exports = method;
